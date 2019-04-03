10b5bc2e0d20b917b81d5e61d2dd6b0b4a2deeb9
Mark Brown <markjbrown@users.noreply.github.com>
2019-04-03 14:13:15 -0700

    Added solution

---
+++ CosmosGlobalDistDemos/CosmosGlobalDistDemos/CustomSynchronization.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace CosmosGlobalDistDemos
+{
+    /*
+    * Resources needed for this demo:
+    * 
+    *   Custom => Cosmos DB account: Replication: Multi-Master, Write Region: West US 2, East US 2, West US, East US, Consistency: Session
+    *   Strong => Cosmos DB account: Replication: Multi-Master, Write Region: West US 2, East US 2, West US, East US, Consistency: Strong
+    *   
+*/
+    public class CustomSynchronization
+    {
+        private string databaseName;
+        private string containerName;
+        private Uri databaseUri;
+        private Uri containerUri;
+        private string PartitionKeyProperty = ConfigurationManager.AppSettings["PartitionKeyProperty"];
+        private string PartitionKeyValue = ConfigurationManager.AppSettings["PartitionKeyValue"];
+        private DocumentClient readClient;
+        private DocumentClient writeClient;
+        private DocumentClient strongClient;
+
+        private List<ResultData> results;
+        private Bogus.Faker<SampleCustomer> customerGenerator = new Bogus.Faker<SampleCustomer>().Rules((faker, customer) =>
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customer.Name = faker.Name.FullName();
+            customer.City = faker.Person.Address.City.ToString();
+            customer.Region = faker.Person.Address.State.ToString();
+            customer.PostalCode = faker.Person.Address.ZipCode.ToString();
+            customer.MyPartitionKey = ConfigurationManager.AppSettings["PartitionKeyValue"];
+            customer.UserDefinedId = faker.Random.Int(0, 1000);
+        });
+
+        public CustomSynchronization()
+        {
+            string endpoint, key, writeRegion, readRegion;
+
+            databaseName = ConfigurationManager.AppSettings["database"];
+            containerName = ConfigurationManager.AppSettings["container"];
+            databaseUri = UriFactory.CreateDatabaseUri(databaseName);
+            containerUri = UriFactory.CreateDocumentCollectionUri(databaseName, containerName);
+            writeRegion = ConfigurationManager.AppSettings["WriteRegion"];
+            readRegion = ConfigurationManager.AppSettings["readRegion"];
+
+            Console.WriteLine("Custom Synchronization for Stronger Consistency without Latency");
+            Console.WriteLine("---------------------------------------------------------------");
+
+            //Shared endpoint and key
+            endpoint = ConfigurationManager.AppSettings["CustomSyncEndpoint"];
+            key = ConfigurationManager.AppSettings["CustomSyncKey"];
+
+            //Write client
+            ConnectionPolicy writePolicy = new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp,
+                UseMultipleWriteLocations = true
+            };
+            writePolicy.SetCurrentLocation(writeRegion);
+            writeClient = new DocumentClient(new Uri(endpoint), key, writePolicy);
+            Console.WriteLine($"Created Write Client with Session consistency in: {writeRegion}.");
+
+            //Read client policy
+            ConnectionPolicy readPolicy = new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp
+            };
+            readPolicy.SetCurrentLocation(readRegion);
+            readClient = new DocumentClient(new Uri(endpoint), key, readPolicy, ConsistencyLevel.Session);
+            Console.WriteLine($"Created Read Client with Session consistency in: {readRegion}.");
+
+            //Strong consistency client
+            ConnectionPolicy strongPolicy = new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp,
+            };
+            strongPolicy.SetCurrentLocation(writeRegion);
+            endpoint = ConfigurationManager.AppSettings["StrongEndpoint"];
+            key = ConfigurationManager.AppSettings["StrongKey"];
+            strongClient = new DocumentClient(new Uri(endpoint), key, strongPolicy, ConsistencyLevel.Strong);
+            Console.WriteLine($"Created client with Strong consistency in: {writeRegion}.");
+            Console.WriteLine();
+
+            writeClient.OpenAsync();
+            readClient.OpenAsync();
+            strongClient.OpenAsync();
+        }
+        public async Task Initalize()
+        {
+            try
+            {
+                Console.WriteLine("Custom Synchronization Initialize");
+                //Database definition
+                Database database = new Database { Id = databaseName };
+
+                //Container definition
+                PartitionKeyDefinition partitionKeyDefinition = new PartitionKeyDefinition();
+                partitionKeyDefinition.Paths.Add(PartitionKeyProperty);
+                DocumentCollection container = new DocumentCollection { Id = containerName, PartitionKey = partitionKeyDefinition };
+
+                //create the database for all accounts
+                await writeClient.CreateDatabaseIfNotExistsAsync(database);
+                await strongClient.CreateDatabaseIfNotExistsAsync(database);
+
+                //Throughput - RUs
+                RequestOptions options = new RequestOptions { OfferThroughput = 1000 };
+
+                //Create the container for all accounts
+                await writeClient.CreateDocumentCollectionIfNotExistsAsync(databaseUri, container, options);
+                await strongClient.CreateDocumentCollectionIfNotExistsAsync(databaseUri, container, options);
+            }
+            catch (DocumentClientException dcx)
+            {
+                Console.WriteLine(dcx.Message);
+                Debug.Assert(false);
+            }
+        }
+        public async Task RunDemo()
+        {
+            try
+            {
+                results = new List<ResultData>();
+
+                Console.WriteLine("Test Latency between Strong Consistency all regions vs. single region");
+                Console.WriteLine("---------------------------------------------------------------------");
+                Console.WriteLine();
+
+                await WriteBenchmarkStrong(strongClient);
+                await WriteBenchmarkCustomSync(writeClient, readClient);
+            }
+            catch (DocumentClientException dcx)
+            {
+                Console.WriteLine(dcx.Message);
+            }
+        }
+        private async Task WriteBenchmarkStrong(DocumentClient client)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            int i = 0;
+            int total = 100;
+            long lt = 0;
+            double ru = 0;
+
+            string region = Helpers.ParseEndpoint(client.WriteEndpoint);
+            string consistency = client.ConsistencyLevel.ToString();
+
+            Console.WriteLine($"Test {total} writes account in {region} with {consistency} consistency between all replicas. \r\nPress any key to continue\r\n...");
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+            for (i = 0; i < total; i++)
+            {
+                SampleCustomer customer = customerGenerator.Generate();
+                stopwatch.Start();
+                ResourceResponse<Document> response = await client.CreateDocumentAsync(containerUri, customer);
+                stopwatch.Stop();
+                Console.WriteLine($"Write: Item {i} of {total}, Region: {region}, Latency: {stopwatch.ElapsedMilliseconds} ms, Request Charge: {response.RequestCharge} RUs");
+                lt += stopwatch.ElapsedMilliseconds;
+                ru += response.RequestCharge;
+                stopwatch.Reset();
+            }
+            results.Add(new ResultData
+            {
+                Test = $"Test with all {consistency} Consistency",
+                AvgLatency = (lt / total).ToString(),
+                AvgRU = Math.Round(ru / total).ToString()
+            });
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Test Summary");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"Test {total} writes account in {region} with {consistency} consistency between all replicas");
+            Console.WriteLine();
+            Console.WriteLine($"Average Latency:\t{(lt / total)} ms");
+            Console.WriteLine($"Average Request Units:\t{Math.Round(ru / total)} RUs");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+        private async Task WriteBenchmarkCustomSync(DocumentClient writeClient, DocumentClient readClient)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            int i = 0;
+            int total = 100;
+            long lt = 0;
+            double ru = 0;
+            long ltAgg = 0;
+            double ruAgg = 0;
+
+            string writeRegion = Helpers.ParseEndpoint(writeClient.WriteEndpoint);
+            string readRegion = Helpers.ParseEndpoint(readClient.ReadEndpoint);
+            string consistency = writeClient.ConsistencyLevel.ToString();
+
+            Console.WriteLine();
+            Console.WriteLine($"Test {total} writes in {writeRegion} with {consistency} consistency between all replicas except {readRegion} with Strong consistency. \r\nPress any key to continue\r\n...");
+            Console.ReadKey(true);
+
+            PartitionKey partitionKeyValue = new PartitionKey(PartitionKeyValue);
+
+            Console.WriteLine();
+            for (i = 0; i < total; i++)
+            {
+                SampleCustomer customer = customerGenerator.Generate();
+
+                stopwatch.Start();
+                    ResourceResponse<Document> writeResponse = await writeClient.CreateDocumentAsync(containerUri, customer);
+                stopwatch.Stop();
+                        lt += stopwatch.ElapsedMilliseconds;
+                        ru += writeResponse.RequestCharge;
+                stopwatch.Reset();
+
+                stopwatch.Start();
+                    ResourceResponse<Document> readResponse = await readClient.ReadDocumentAsync(writeResponse.Resource.SelfLink, 
+                        new RequestOptions { PartitionKey = partitionKeyValue, SessionToken = writeResponse.SessionToken});
+                stopwatch.Stop();
+                        lt += stopwatch.ElapsedMilliseconds;
+                        ru += readResponse.RequestCharge;
+                stopwatch.Reset();
+                Console.WriteLine($"Write/Read: Item {i} of {total}, Region: {writeRegion}, Latency: {lt} ms, Request Charge: {ru} RUs");
+
+                ltAgg += lt;
+                ruAgg += ru;
+                lt = 0;
+                ru = 0;
+            }
+            results.Add(new ResultData
+            {
+                Test = $"Test with Custom Synchronization",
+                AvgLatency = (ltAgg / total).ToString(),
+                AvgRU = Math.Round(ruAgg / total).ToString()
+            });
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Test Summary");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"Test {total} writes in {writeRegion} with {consistency} consistency between all replicas except {readRegion} with Strong consistency");
+            Console.WriteLine();
+            Console.WriteLine($"Average Latency:\t{(ltAgg / total)} ms");
+            Console.WriteLine($"Average Request Units:\t{Math.Round(ruAgg / total)} RUs");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+             
+            Console.WriteLine();
+            Console.WriteLine("All Tests Summary");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            foreach (ResultData r in results)
+            {
+                Console.WriteLine($"{r.Test}\tAvg Latency: {r.AvgLatency} ms\tAverage RU: {r.AvgRU}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Test concluded. Press any key to continue\r\n...");
+            Console.ReadKey(true);
+        }
+        public async Task CleanUp()
+        {
+            try
+            {
+                await writeClient.DeleteDatabaseAsync(databaseUri);
+                await strongClient.DeleteDatabaseAsync(databaseUri);
+            }
+            catch { }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,57 +1,60 @@
-# Project Name
+# Cosmos Global Distribution Demos
 
-(short, 1-3 sentenced, description of the project)
+## Introduction
 
-## Features
+This solution contains a series of benchmarks that demonstrate various concepts for distributed databases, particularly around consistency, latency and distance. The tests in this solution include:
 
-This project framework provides the following features:
+### 1. Read latency between single region and multi-region replicated accounts
 
-* Feature 1
-* Feature 2
-* ...
+This test shows the difference in read latency for an account with a single master in SouthEast Asia region with a front end reading from it in West US 2. The next test shows the drastic improvement in latency with data locality when the account is replicated to West US 2.
 
-## Getting Started
+### 2. Write latency for accounts with Eventual consistency vs. Strong consistency + impact of distance on Strong consistency
 
-### Prerequisites
+This test shows the difference in write latency for two accounts with replicas 1000 miles apart in West US 2 and Central US regions, one with Eventual consistency, the other with Strong consistency. There is a third test that shows the impact on latency when the distance between the regions is more than double the distance, demonstrating the speed of light impact on latency when using Strong consistency across large distances.
 
-(ideally very short, if any)
+### 3. Read and write latency for Single-Master account versus Multi-Master account
 
-- OS
-- Library version
-- ...
+This test shows the difference in read latency for a single-master account (master: East US 2, replica: West US 2) with a client in West US 2. The next test shows the impact on write latency when using a multi-master account (master: East US 2, West US 2) with a client in West US 2.
 
-### Installation
+### 4. Multi-Master Conflict Resolution
 
-(ideally very short)
+This test shows the Last Write Wins and Merge Procedure conflict resolution modes as well as "Async" mode where conflicts are written to the Conflicts Feed.
 
-- npm install [package name]
-- mvn install
-- ...
+### 5. Custom Synchronization
 
-### Quickstart
-(Add steps to get up and running quickly)
+This test shows how to implement a custom synchronization between two regions. This allows you to have a lower level of consistency for a database with many replicas across great distances. This scenario shows an account with four regions (West US, West US 2, East US, East US 2) at Session level consistency but with Strong consistency between West US and West US 2. This provides for greater data durability (RPO = 0) without having to use Strong consistency across all regions and over very large distances. This demo includes a separate class that shows a simpler implementation of this you can more easily use without all the timer code.
 
-1. git clone [repository clone url]
-2. cd [respository name]
-3. ...
+## Provisioning Cosmos DB accounts
 
+This solution requires nine different Cosmos DB accounts. Each of are configured differently to support the underlying test with different replication modes, consistency levels and regions.
 
-## Demo
+To deploy the Cosmos DB accounts for this solution, follow the steps below.
 
-A demo app is included to show how to use the project.
+### Steps
 
-To run the demo, follow these steps:
+- Click "Deploy to Azure" below
+- Enter a resource group name and location
+- Enter a unique string to act as a prefix to make your Cosmos DB accounts unique
+- When the script is complete, click on the Outputs tab
+- Open the app.config for the solution on your local machine
+- Copy the values for the endpoints and keys from each account to their corresponding placeholder in app.config
 
-(Add steps to start up the demo)
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fmarkjbrown%2Fcosmos-global-distribution-demos%2Fmaster%2FCosmosGlobalDistDemos%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
 
-1.
-2.
-3.
+## Provision Windows VM as Host
 
-## Resources
+These tests are designed to run from a Windows VM in West US 2. You will need a Windows VM with 2-4 CPU's and 8-16 GB of memory with RDP enabled. After the VM has been provisioned, RDP into it and install Visual Studio 2017, then copy the solution folder to the VM, or connect VS to your forked repo and clone it locally to the VM. Alternatively you can use the [Data Science VM](https://ms.portal.azure.com/#create/microsoft-dsvm.dsvm-windowsserver-2016) and just follow the remaining steps to copy the solution to that VM.
 
-(Any additional resources or related projects)
+To run the demo, RDP into the VM, open the solution folder, launch the solution and press F5.
 
-- Link to supporting information
-- Link to similar sample
-- ...
+[!Note]
+> You can run this demo from your local machine however the latency benchmarks will be dramatically slower and will not show actual, SLA-based Cosmos DB latency metrics.
+
+## Initializing the Demos
+
+After the accounts are provisioned you can launch the application. Before running any demos you must run the "Initialize" menu item first. Running Initialize will provision 9 databases and 11 containers. Throughput is set at the container level at 1000 RU/s.
+
+[!IMPORTANT]
+> This solution has 11 containers provisioned at 1000 RU/s each which can be quite expensive if left provisioned. It is recommended that you run "Initialize" each time you run this solution and then run "Clean up" when you are done which will delete all the databases and containers for this sample. This will reduce your costs to the absolute minimum. Also be careful not to leave the VM running if not being used.
